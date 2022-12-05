9fd94ff8073b2d48429dd78f70bff3f4c427ceab
p <s>
2022-12-05 12:58:03 +0100

    Favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
-import About from "../components/About";
-import Banner from "../components/Banner";
-import Categories from "../components/Categories";
-import Conditions from "../components/Conditions";
-import FAQs from "../components/FAQs";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
-import SaleProcess from "../components/SaleProcess";
-import Testimonial from "../components/Testimonial";
-import WhatWeBuy from "../components/WhatWeBuy";
+import React from "react"
+import About from "../components/About"
+import Banner from "../components/Banner"
+import Categories from "../components/Categories"
+import Conditions from "../components/Conditions"
+import FAQs from "../components/FAQs"
+import Footer from "../components/Footer"
+import Nav from "../components/Nav"
+import SaleProcess from "../components/SaleProcess"
+import Testimonial from "../components/Testimonial"
+import WhatWeBuy from "../components/WhatWeBuy"
+
+import CookieConsent from "react-cookie-consent"
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Nav />
-      <Banner/>
-      <Categories/>
-      <SaleProcess/>
-      <WhatWeBuy/>
-      <About/>
-      <Testimonial/>
-      <FAQs/>
-      <Conditions/>
-      <Footer/>
+      <Banner />
+      <Categories />
+      <SaleProcess />
+      <WhatWeBuy />
+      <About />
+      <Testimonial />
+      <FAQs />
+      <Conditions />
+      <Footer />
+
+      <CookieConsent
+        enableDeclineButton
+        flipButtons
+        location="bottom"
+        buttonText="Annehmen"
+        declineButtonText="Ablehnen"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        declineButtonStyle={{
+          color: "white",
+          background: "#bfbfbf",
+          fontSize: "15px",
+          borderRadius: "10px",
+          fontWeight: "lighter",
+        }}
+        buttonStyle={{
+          color: "white",
+          background: "#03a84e",
+          fontSize: "20px",
+          borderRadius: "10px",
+          fontWeight: "normal",
+        }}
+        expires={150}
+      >
+        Unsere Website verwendet Cookies um eine optimale Benutzererfahrung zu
+        bieten.
+        {/* <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span> */}
+      </CookieConsent>
     </div>
-  );
+  )
 }
